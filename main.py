2728235fa194f5bf270b4868da8cbb4f278c36e3
Viktor i9p <109454662+SS56Bo@users.noreply.github.com>
2022-07-17 10:14:59 +0000

    Added some few lines, will start adding music soon

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import discord
 import os
+import random
+from discord.ext import commands
 client = discord.Client()
+
+def get_quote():
+    file=open('quote.txt','r')
+    opn=file.read().splitlines()
+    return (random.choice(opn))
 
 @client.event
 async def on_ready():
@@ -15,8 +22,17 @@ async def on_message(message):
     await message.channel.send("Hi, I'm Pat Botman")
 
   if message.content.startswith('give me a quote'):
-    await message.channel.send("")
+    await message.channel.send(get_quote())
+
+  if message.content.startswith('give me another'):
+    await message.channel.send(get_quote())
 
 
+  if message.content.startswith('play some music'):
+    await message.channel.send("Do you like Huey Lewis and the News?")
+
+
+
+  
 client.run(os.environ['tokken'])
   
